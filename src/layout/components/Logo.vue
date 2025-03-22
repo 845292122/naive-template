2307055b965bc84323fa9d427c80f649537dfeb5
edison <845292122@qq.com>
2025-03-22 15:19:38 +0800

    feat: logo

---
+++ src/layout/components/Logo.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Logo from '~/assets/vue.svg'
+
+const title = import.meta.env.VITE_APP_TITLE
+</script>
+
+<template>
+  <div flex items-center>
+    <img :src="Logo" alt="logo" mr-7 />
+    <n-gradient-text type="primary" size="18"> {{ title }} </n-gradient-text>
+  </div>
+</template>
--- src/layout/default/index.vue
+++ src/layout/default/index.vue
@@ -1,107 +1,34 @@
 <script setup>
-import { AccountBookFilled, AimOutlined } from '@vicons/antd'
-import { renderIcon } from '~/utils'
+import SideMenu from '../components/SideMenu'
+import Logo from '../components/Logo.vue'
+import { useAppStore } from '~/store'
 
-const testOptions = [
-  {
-    label: 'Home',
-    key: 'home',
-    icon: renderIcon(AccountBookFilled)
-  },
-  {
-    label: 'Test',
-    key: 'test',
-    icon: renderIcon(AimOutlined),
-    children: [
-      {
-        label: 'Test1',
-        key: 'test1'
-      },
-      {
-        label: 'Test2',
-        key: 'test2'
-      }
-    ]
-  }
-]
+const appStore = useAppStore()
 </script>
 
 <template>
-  <!-- <div style="position: relative" h-full>
-    <n-layout has-sider wh-full position="absolute">
-      <n-layout-sider collapse-mode="transform" :collapsed-width="60" :width="220" show-trigger="bar" bordered>
-        <div flex-col>
-          <div w-full items-center h-56 flex justify-center>logo</div>
-          <n-menu :options="testOptions" />
-        </div>
-      </n-layout-sider>
-      <n-layout position="absolute">
-        <n-layout-header h-56>颐和园路</n-layout-header>
-        <n-layout-content>
-          dsfaksflsafj
-          <h1>12sdfsdfsdfsdfdsfdsfdsfdskfaskdjfalsfjlskj3</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-          <h1>123</h1>
-        </n-layout-content>
-      </n-layout>
-    </n-layout>
-  </div> -->
-
   <n-layout h-full>
     <n-layout-header style="height: 64px" bordered px-20>
       <div flex justify-between wh-full items-center>
-        <!-- TODO logo组件 -->
-        <div>TODO: logo</div>
+        <Logo />
         <!-- TODO action组件 -->
         <div>TODO: action</div>
       </div>
     </n-layout-header>
     <n-layout position="absolute" style="top: 64px" has-sider>
-      <n-layout-sider collapse-mode="transform" :collapsed-width="60" :width="220" show-trigger="bar" bordered>
-        <div flex-col>
-          <!-- TODO menu组件 -->
-          <n-menu :options="testOptions" />
-        </div>
+      <n-layout-sider
+        :width="220"
+        show-trigger="true"
+        bordered
+        collapse-mode="width"
+        :collapsed="appStore.menuCollapsed"
+        :collapsed-width="64"
+        :collapsed-icon-size="20"
+        @update:collapsed="appStore.toggleMenuCollapsed"
+      >
+        <SideMenu />
       </n-layout-sider>
+
       <n-layout content-style="padding: 24px;" :native-scrollbar="false">
         <n-h2>平山道</n-h2>
         <n-h2>平山道</n-h2>
